b1baf71b4450882528b8c58c4f1d27c3f59cf664
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-12-20 20:02:43 -0300

    implementa conexao socket

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
-const express = require("express");
-const app = express();
+const app = require("express")();
+const http = require("http").createServer(app);
 const cors = require("cors");
-app.use(express.json());
 app.use(cors());
 
-const port = 3000;
-
-app.get("/", (req, res) => {
-  res.send("Api is running ...");
+const io = require("socket.io")(http, {
+  allowEIO3: true,
+  cors: {
+    origin: true,
+    credentials: true,
+  },
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+const port = 3000;
+http.listen(port, () => {
+  console.log(`listening on http://localhost:${port}`);
+});
+
+io.sockets.on("connection", (socket) => {
+  console.log("user connected", socket.id);
+
+  socket.on("test-connected-user", (fn) => {
+    fn("Test user conectado " + socket.id);
+  });
 });
